19360dc276dd79c7812e7d588aa51bdb442a3e37
Guilherme Anschau <anschau.gui@gmail.com>
2025-01-28 10:13:41 -0300

    atualização

--- bank/cliente.js
+++ bank/cliente.js
@@ -1,24 +1,40 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 new Cliente();
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = 'Alice';
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 100;
+console.log(`o saldo do ${cliente1.nome} é: ${contaCorrenteRicardo.saldo}`);
+contaCorrenteRicardo.sacar(220);
+console.log(`o saldo do ${cliente1.nome} é: ${contaCorrenteRicardo.saldo}`);
+
+
+const ContaCorrenteAlice = new ContaCorrente();
+ContaCorrenteAlice.agencia = 1002;
+ContaCorrenteAlice.saldo = 0;
 
 
 
